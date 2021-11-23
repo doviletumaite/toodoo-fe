439a18cad9dd1514ed0c61a0426666dc203a0d62
Dovile Tumaite <dovile.tumaite@yahoo.it>
2021-11-23 17:47:35 +0100

    login style

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,30 +1,47 @@
-import NavBar from "../components/NavBar"
-import "../style/Login.css"
+import NavBar from "../components/NavBar";
+import "../style/Login.css";
+import google from "../style/images/google.png";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-    return(
-        <div>
-           <NavBar/>  
-        <div className="loginBox">
-            <div className="loginTitle">login on your profile</div>
-            <div className="input-container">
-                <div className="input-controler">
-                <label>your name</label>
-                <input className="input"></input>
-                </div>
-                <div className="input-controler">
-                <label>your email</label>
-                <input className="input"></input>
-                </div>
-                <div className="input-controler">
-                <label> password </label>
-                <input className="input"></input>
-                </div>
-                <button className="Google-button">Sign in with Google</button>
-            </div>
+  return (
+    <div>
+      <NavBar />
+      <div className="loginBox">
+        <div className="loginTitle">login on your profile</div>
+        <div className="input-container">
+          <div className="input-controler">
+            <label>your name</label>
+            <input className="input"></input>
+          </div>
+          <div className="input-controler">
+            <label>your email</label>
+            <input className="input"></input>
+          </div>
+          <div className="input-controler">
+            <label> password </label>
+            <input className="input"></input>
+          </div>
         </div>
-        </div>
-    )
-    }
-    
-    export default Login
+<div className="buttonsGroup">
+        <a href="">
+          <button className="loginButton">login</button>
+        </a>
+        <a href="">
+          <button className="googleButton">
+            <img src={google} />
+            Sign in with Google
+          </button>
+        </a>
+
+        <div className="registerString">not registered yet?</div>
+        <Link to="/" className="registerLink">
+          <button className="registerButton">create a new account!</button>
+        </Link>
+</div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
