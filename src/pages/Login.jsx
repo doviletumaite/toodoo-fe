2944c662f79505900bc11a12835d224d97adbefa
Dovile Tumaite <dovile.tumaite@yahoo.it>
2021-11-23 17:13:57 +0100

    login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -6,7 +6,22 @@ const Login = () => {
         <div>
            <NavBar/>  
         <div className="loginBox">
-      
+            <div className="loginTitle">login on your profile</div>
+            <div className="input-container">
+                <div className="input-controler">
+                <label>your name</label>
+                <input className="input"></input>
+                </div>
+                <div className="input-controler">
+                <label>your email</label>
+                <input className="input"></input>
+                </div>
+                <div className="input-controler">
+                <label> password </label>
+                <input className="input"></input>
+                </div>
+                <button className="Google-button">Sign in with Google</button>
+            </div>
         </div>
         </div>
     )
