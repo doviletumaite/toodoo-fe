0e04863ee1671cc393e8f6db5cf7863473321e8a
Dovile Tumaite <dovile.tumaite@yahoo.it>
2021-11-25 17:43:33 +0100

    api tools

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,8 +2,20 @@ import NavBar from "../components/NavBar";
 import "../style/Login.css";
 import google from "../style/images/google.png";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {create} from "axios"
 
 const Login = () => {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [username, setUsername] = useState("")
+  const [query, setQuery] = useState("")
+
+  const dispatch = useDispatch()
+  const data = useSelector((s) => s)
+
+  const URL = create({baseURL: "http://localhost:3003"})
   return (
     <div>
       <NavBar />
@@ -23,22 +35,22 @@ const Login = () => {
             <input className="input"></input>
           </div>
         </div>
-<div className="buttonsGroup">
-        <a href="">
-          <button className="loginButton">login</button>
-        </a>
-        <a href="">
-          <button className="googleButton">
-            <img src={google} />
-            Sign in with Google
-          </button>
-        </a>
+        <div className="buttonsGroup">
+          <a href="">
+            <button className="loginButton">login</button>
+          </a>
+          <a href="http://localhost:3003/user/googleLogin">
+            <button className="googleButton">
+              <img src={google} />
+              Sign in with Google
+            </button>
+          </a>
 
-        <div className="registerString">not registered yet?</div>
-        <Link to="/register" className="registerLink">
-          <button className="registerButton">create a new account!</button>
-        </Link>
-</div>
+          <div className="registerString">not registered yet?</div>
+          <Link to="/register" className="registerLink">
+            <button className="registerButton">create a new account!</button>
+          </Link>
+        </div>
       </div>
     </div>
   );
