d0c156c94b1a3892d851a3d936fb6831483c9624
Dovile Tumaite <dovile.tumaite@yahoo.it>
2021-12-08 18:52:43 +0100

    lists

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -4,7 +4,12 @@ import "../style/ProfilePage.css"
 
 const ProfilePage = () => {
     const state = useSelector(s=>s.userInfo)
-  
+    const date = new Date()
+    const dd = String(date.getDate()).padStart(2, '0');
+    const mm = String(date.getMonth() + 1).padStart(2, '0');
+    const yyyy = date.getFullYear();
+
+    const today = dd + '/' + mm + '/' + yyyy;
     return (
         <div>
             <NavBar/>
@@ -20,7 +25,11 @@ const ProfilePage = () => {
                         <p className="userInfoBox_profileName" >{state.username}</p>
                         <p className="userInfoBox_bio" >{state.bio}ciao</p>
                         <button className="editInfoButton">edit personal info</button>
-
+                        <p className="calendarsTitle">my calendars:</p>
+                        <div className="calendarLabel"> GIM </div>
+                        <div className="calendarLabel"> SCHOOL </div>
+                        <p className="addCalendar">add a new calendar</p>
+                        <input className="addCalendarInput" type="text"/>
                         </div>
                    </div>
                 </div>
@@ -28,7 +37,23 @@ const ProfilePage = () => {
 
                 {/* list side */}
                 <div className="listSession">
+                <div className="dailyList">
+                <p className="list-Title">GIM</p>
+                  <div className="list">
+                  <p className="date">today's date: {today}</p>   
+                <p className="dailyGoals">my daily goals:</p>
 
+                     <div className="inputs-list">
+                <input type="text" placeholder="add some tasks!"/>
+                    </div>
+
+                    <div className="checks">
+                <input className="checkbox" type="checkbox"/>
+                <label className="label">run 30 min</label>
+                     </div>
+                  </div>
+                </div>
+                <div className="calendar"></div>
                 </div>
             </div>
         </div>
